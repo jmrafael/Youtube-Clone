a4bb7d462fee99077be2290c60def6a7478b0411
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2022-06-18 23:42:08 +0200

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,4 +1,82 @@
 <template>
-  A
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="white"
+        padding
+        arrows
+        height="50px"
+        class="bg-primary text-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" class="column no-wrap flex-center">
+<!--          <q-icon name="style" size="16px" />-->
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+<!--          <q-icon name="live_tv" size="16px" />-->
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+<!--          <q-icon name="layers" size="56px" />-->
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+<!--          <q-icon name="terrain" size="56px" />-->
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
+  </div>
+
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col">
+        <q-card class="my-card">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card class="my-card">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+  </div>
+
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref('style'),
+      lorem: 'Categorias aqui'
+    }
+  }
+}
+</script>
 
